f25bdabb97ce4c4b0e00456ab0d56916a2ea72e8
iavci <iavci@LAPTOP-4674SJUE>
2025-03-16 15:21:36 +0300

    Add project files.

---
+++ erisim_belirleyici/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+using Erisim_Belirleyici.Erisim;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        User kullanici = new User();
+
+        kullanici.SetAdSoyad("İlayda Avcı");
+        kullanici.SetYas(20);
+        kullanici.Email = "ilaydaavci26@gmail.com";
+
+        kullanici.BilgileriGoster();
+    }
+}
+
+//Erişim Belirleyicilerin OOP’deki Rolü
+//1)  Kapsülleme (Encapsulation): Veri saklama ve güvenliğini sağlar. private ve protected kullanımıyla dış erişim sınırlandırılır.
+//2)  Kodun Yönetilebilirliğini Artırma: public ve internal ile modüler bir yapı oluşturulabilir, kod tekrarını önler.
+//3)  Miras (Inheritance) Kontrolü: protected ile alt sınıflara özel erişim tanımlanabilir.
+//4)  Daha Güvenli Yazılım Geliştirme: Hassas verilerin (private) dışarıdan değiştirilmesini önler, böylece hatalara ve saldırılara karşı koruma sağlar.
+//
+//1. public (Genel):Bir üyeye(metod, değişken vb.) herkes erişebilir. Farklı sınıflardan veya projelerden çağrılabilir.
+//2. private (Özel): Sadece tanımlandığı sınıf içerisinden erişilebilir. Dışarıdan erişime kapalıdır
+//3. protected (Korunan): Sadece tanımlandığı sınıf ve o sınıftan türeyen alt sınıflar tarafından erişilebilir.
+//4. internal (Dahili): Aynı proje(assembly) içerisindeki tüm sınıflardan erişilebilir, ancak farklı projelerden erişilemez.
---
+++ erisim_belirleyici/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erisim_Belirleyici.Erisim
+{
+    public class User
+    {
+        private string adSoyad;
+        private int yas;
+
+        public string Email { get; set; }
+
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+        public void SetAdSoyad(string yeniAdSoyad)
+        {
+            if (!string.IsNullOrWhiteSpace(yeniAdSoyad)) // Boş değer engellendi
+            {
+                adSoyad = yeniAdSoyad;
+            }
+            else
+            {
+                Console.WriteLine("Ad Soyad boş olamaz!");
+            }
+        }
+        public int GetYas()
+        {
+            return yas;
+
+        }
+        public void SetYas(int yeniYas)
+        {
+            yas = yeniYas;
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Ad Soyad: {adSoyad}");
+            Console.WriteLine($"Yaş: {yas}");
+            Console.WriteLine($"E-mail: {Email}");
+        }
+    }
+}
